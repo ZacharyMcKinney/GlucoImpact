35b22677224898b36a935f2da166598046a38d74
ZacharyMcKinney <Zachary.Mckinney04@gmail.com>
2025-06-28 23:53:27 -0500

    Add a Food_Item dataclass for storing the ID, name and macros of a food. Useful for when adding to DB

---
+++ src/food_item.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+@dataclass
+class Food_Item:
+    """Class for keeping track of a food's ID, name, and macros per serving"""
+    food_id: int
+    food: str
+    carbs: float
+    protein: float
+    fat: float
+    
