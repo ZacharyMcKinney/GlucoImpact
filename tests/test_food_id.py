0da2b480504abbeb96f1ee62f21f0832cb262198
ZacharyMcKinney <Zachary.Mckinney04@gmail.com>
2025-07-16 13:32:03 -0500

    Added image_hash function and commented out fxns

--- tests/test_food_id.py
+++ tests/test_food_id.py
@@ -1,16 +1,17 @@
 import unittest
 import src.food_id as fid
+import hashlib
 
 # --- TODO LIST ---
 # Test a "None" picture that is unidentifiable 
 
 class TestFoodID(unittest.TestCase):
 
-    def setUp(self):
-        pass
+    # def setUp(self):
+    #     pass
 
-    def test_get_img_location(self):
-        pass
+    # def test_get_img_location(self):
+    #     pass
     
     def test_get_img(self):
         pass
@@ -32,6 +33,9 @@ class TestFoodID(unittest.TestCase):
 
     def test_load_openai_prompts(self) :
         pass
+
+def _image_hash(img):
+    return hashlib.md5(img.tobytes()).hexdigest()
 
 if __name__ == "__main__":
     unittest.main()
