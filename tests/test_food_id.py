b0834303a6c2bba70e68d1ad89055ac5b7b62c6b
ZacharyMcKinney <Zachary.Mckinney04@gmail.com>
2025-07-15 21:52:23 -0500

    Added function defintions for each function

---
+++ tests/test_food_id.py
@@ -0,0 +1,37 @@
+import unittest
+import src.food_id as fid
+
+# --- TODO LIST ---
+# Test a "None" picture that is unidentifiable 
+
+class TestFoodID(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_get_img_location(self):
+        pass
+    
+    def test_get_img(self):
+        pass
+    
+    def test_is_supported(self):
+        pass
+    
+    def test_convert_img(self):
+        pass
+    
+    def test_convert_pil_to_base64(self):
+        pass
+
+    def test_identify_food(self):
+        pass
+
+    def test_identify_foods_data(self):
+        pass
+
+    def test_load_openai_prompts(self) :
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
