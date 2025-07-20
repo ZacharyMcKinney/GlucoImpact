c20138523d0cafbc9c2a1d6029ef443265b31213
ZacharyMcKinney <Zachary.Mckinney04@gmail.com>
2025-07-20 00:28:13 -0500

    Added food_id function defs for get_img

--- tests/test_food_id.py
+++ tests/test_food_id.py
@@ -1,19 +1,36 @@
 import unittest
 import src.food_id as fid
 import hashlib
+from PIL import Image
 
 # --- TODO LIST ---
 # Test a "None" picture that is unidentifiable 
 
 class TestFoodID(unittest.TestCase):
 
-    # def setUp(self):
-    #     pass
+    def setUp(self):
+        img1_str = "./images/single_foods/apple.jpg"
+        img1 = Image.open(img1_str)
+        img2 = Image.open("./images/single_foods/bagels.jpg")
+        img3 = Image.open("./images/single_foods/yogurt.jpg")
+        img4 = Image.open("./images/single_foods/mashed_potatoes.jpg")
+        img5 = Image.open("./images/multiple_foods/bibimbap.jpg")
+        img6 = Image.open("./images/multiple_foods/salmon_dinner.jpg")
+        img7 = Image.open("./images/multiple_foods/yogurt_parfait.jpg")
+        img9 = Image.open("./images/multiple_foods/turkey_sandwich.jpg")
 
     # def test_get_img_location(self):
     #     pass
     
-    def test_get_img(self):
+    def test_get_img_pass(self):
+        expected_hash = self.image_hash(self.img1)
+        result_hash = fid.get_img(self.img1_str)
+        self.assertEqual(expected_hash, result_hash)
+    
+    def test_get_img_not_found(self):
+        pass
+
+    def test_get_img_unidentified(self):
         pass
     
     def test_is_supported(self):
@@ -34,8 +51,8 @@ class TestFoodID(unittest.TestCase):
     def test_load_openai_prompts(self) :
         pass
 
-def _image_hash(img):
-    return hashlib.md5(img.tobytes()).hexdigest()
+    def image_hash(img: Image.Image):
+        return hashlib.md5(img.tobytes()).hexdigest()
 
 if __name__ == "__main__":
     unittest.main()
