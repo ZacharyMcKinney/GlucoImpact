98cb0ffe269a480234ea0889910a5be91e7e627b
ZacharyMcKinney <Zachary.Mckinney04@gmail.com>
2025-07-25 23:22:12 -0500

    Added 1 table and 4 delete funcs and revised meals Meals are now reusable that many users can have. Consumption is now a log with its own id

--- src/db_manager.py
+++ src/db_manager.py
@@ -1,15 +1,17 @@
 ## Copyright 2025, Zachary McKinney
 import sqlite3
-from food_item import Food_Item
+from models import Food_Item, Meal, User
 from datetime import date
 
-
-# Note to Self: Implement crud? create, retrieve, update, delete functions
-
-#This class will be the only one allowed to access the database
-#bgl_analyzer will put these functions together into one
-
 class DB_Manager:
+    """_summary_
+
+    Raises:
+        TypeError: _description_
+
+    Returns:
+        _type_: _description_
+    """
     
 # --- Class tools and Initiation ---
        
@@ -24,52 +26,65 @@ class DB_Manager:
         self._cursor = self._connection.cursor()
         self._cursor.execute("PRAGMA foreign_keys = ON;")
         self.create_tables()
-        self.cursor.commit()
+        self._cursor.commit()
     
     def close_cursor(self) -> None:
         """
         Closes database cursor connection.
         __Init__ doesn't close it after intiating the cursor
         """
-        self.cursor.close()
+        self._cursor.close()
     
     def create_tables(self) -> None:
         """
-        Creates tables if they don't exist int the database.
-        Has four tables in the database.
-        users, meal_consumed, food, and meal_items.
+        Creates tables if they don't exist in the database.
+        Has four tables in the database:
+        users, meal_consumed, food, meals, and meal_items.
+        
+        users: data associated with a person
+        meal_consumed: log for food consumed and bgl data
+        food: information regarding a single food item
+        meals: reusable key for multiple meal_items
+        meal_items: macro portions in a meal
         """
         
-        self.cursor.execute("""
+        self._cursor.execute("""
                            CREATE TABLE IF NOT EXISTS users(
-                               user_id INTEGER PRIMARY KEY, 
+                               user_id INTEGER PRIMARY KEY AUTOINCREMENT, 
                                name TEXT)
                            """)
-        self.cursor.execute("""
+        self._cursor.execute("""
                            CREATE TABLE IF NOT EXISTS meal_consumed(
-                               meal_id INTEGER PRIMARY KEY, 
+                               consumption_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                               meal_id INTEGER, 
                                user_id INTEGER, 
                                bgl_delta REAL, 
                                date TEXT, 
                                time_of_day TEXT,
-                               FOREIGN KEY (user_id) REFERENCES users(user_id))
+                               FOREIGN KEY (meal_id) REFERENCES meals(meal_id))
+                               FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE)
                            """)
-        self.cursor.execute("""
+        self._cursor.execute("""
                            CREATE TABLE IF NOT EXISTS food(
-                               food_id INTEGER PRIMARY KEY,
+                               food_id INTEGER PRIMARY KEY AUTOINCREMENT,
                                food TEXT,
                                carbs REAL,
                                protein REAL,
                                fat REAL)
                            """)
-        self.cursor.execute("""
+        self._cursor.execute("""
+                           CREATE TABLE IF NOT EXISTS meals(
+                               meal_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                               name TEXT)
+                           """)
+        self._cursor.execute("""
                            CREATE TABLE IF NOT EXISTS meal_items(
                                meal_id INTEGER, 
                                food_id INTEGER, 
                                portion INTEGER DEFAULT 1,
                                PRIMARY KEY (meal_id, food_id),
-                               FOREIGN KEY (meal_id) REFERENCES meal_consumed(meal_id),
-                               FOREIGN KEY (food_id) REFERENCES food(food_id))
+                               FOREIGN KEY (meal_id) REFERENCES meals(meal_id) ON DELETE CASCADE,
+                               FOREIGN KEY (food_id) REFERENCES food(food_id) ON DELETE CASCADE)
                            """)
         
 # --- Add / Insert Functions ---
@@ -91,11 +106,11 @@ class DB_Manager:
         self._db_add_food(food)
             
     def _db_add_food(self, food: Food_Item) -> None:
-        self.cursor.execute("""
+        self._cursor.execute("""
                             INSERT INTO food({food}, {carbs}, {protein}, {fat})
                             VALUES (?, ?, ?, ?)
                             """, (food.food, food.carbs, food.protein, food.fat))
-        self._db_connection.commit()   
+        self._connection.commit()   
         
     def _add_meal_items(self) -> None:
         pass
@@ -129,8 +144,6 @@ class DB_Manager:
     def get_all_meals(self) -> tuple[Meal]:
         pass
 
-
-
     #return the BGl for a food (only values, no dates) 
     def _get_food_BGL_data(self, food_id: int) -> float:
         pass
@@ -154,11 +167,21 @@ class DB_Manager:
 
 # --- Delete Functions ---
 
-    def delete_food(self, food_id: int) -> Food_Item:
-        pass
+    def delete_user(self, user_id: int):
+        self._cursor.execute("DELETE FROM users WHERE user_id = ?", (user_id,))
+        self._connection.commit()
+    
+    def delete_food(self, food_id: int):
+        self._cursor.execute("DELETE FROM food WHERE food_id = ?", (food_id,))
+        self._connection.commit()
 
-    def delete_meal(self, meal_id: int) -> Meal:
-        pass
+    def delete_meal(self, meal_id: int):
+        self._cursor.execute("DELETE FROM meals WHERE meal_id = ?", (meal_id,))
+        self._connection.commit()
+        
+    def delete_meal_consumed(self, consumption_id: int):
+        self._cursor.execute("DELETE FROM meal_consumed WHERE meal_id = ?", (consumption_id,))
+        self._connection.commit()
 
 # --- Utilities ---       
         
