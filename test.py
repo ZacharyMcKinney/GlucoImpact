400125bf6ea36ebf0b8b651ddc6f0294a7f3fa2e
ZacharyMcKinney <Zachary.Mckinney04@gmail.com>
2025-07-30 00:04:39 -0500

    Added logging to test runner files

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
 import unittest
+import logging
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.ERROR,
+        format="%(asctime)s %(levelname)s %(name)s: %(message)s",
+    )
+    logging.getLogger("food_id").setLevel(logging.DEBUG)
+    logging.getLogger("test_food_id").setLevel(logging.DEBUG)
+    
     unittest.TextTestRunner(verbosity=2).run(
         unittest.defaultTestLoader.discover("tests")
     )
