a2017d6036a970ea7f0d12a3ab518b0315e0197c
ZacharyMcKinney <Zachary.Mckinney04@gmail.com>
2025-07-29 23:53:56 -0500

    Modified the base64 test to check based on avg and percentile difference on pixels to make sure image quality was preserved and most pixels did not have a large difference

--- tests/test_food_id.py
+++ tests/test_food_id.py
@@ -2,6 +2,8 @@ import unittest
 import src.food_id as fid
 import hashlib
 from PIL import Image
+import logging
+log = logging.getLogger("test_food_id")
 
 # --- TODO LIST ---
 # Test a "None" picture that is unidentifiable 
@@ -17,7 +19,7 @@ class TestFoodID(unittest.TestCase):
         self.img4 = "./images/single_foods/mashed_potatoes.jpg"
         self.img5 = "./images/multiple_foods/bibimbap.jpg"
         self.img6 = "./images/multiple_foods/salmon_dinner.jpg"
-        self.img7 = "./images/multiple_foods/yogurt_parfait.jpg"
+        self.img7 = "./images/multiple_foods/yogurt_parfait.webp"
         self.img9 = "./images/multiple_foods/turkey_sandwich.jpg"
         # false_path = "./images/multiple_foods/fake.jg"
         self.unsupported_path = "./tests/test_assets/gif_image.gif"
@@ -52,18 +54,37 @@ class TestFoodID(unittest.TestCase):
         self.assertEqual("WEBP", fid.convert_img(fid.get_img(self.img1), "WEBP").format)
     
     def test_convert_pil_to_base64(self):
-        self.assertEqual(
-            self.get_text("/test_assets/apple_b64.txt"),
-            fid.convert_pil_to_base64(fid.get_img("./images/single_foods/apple.jpg"))
-                         )
-        self.assertEqual(
-            self.get_text("./test_assets/egg_toast_b64.txt"),
-            fid.convert_pil_to_base64(fid.get_img("./images/single_foods/apple.jpg"))
-                         )
-        self.assertEqual(
-            self.get_text("./test_assets/rice_b64.txt"),
-            fid.convert_pil_to_base64(fid.get_img("./images/single_foods/rice.jpg"))
-                         )
+        AVG_PIX_DIF = 5
+        MAX_PERCENTILE_DIF = 30
+        
+        imgs = [self.img1, self.img6, self.img7]
+        for img in imgs:
+            log.info(f"Test img {img} being converted")
+            avg_diff, percentile_diff = self.b64_avg_and_percentile_diff(img)
+            self.assertLessEqual(avg_diff, AVG_PIX_DIF)
+            self.assertLessEqual(percentile_diff, MAX_PERCENTILE_DIF)
+            log.info(f"Passed avg and percentile test for {img}")
+
+    @staticmethod
+    def b64_avg_and_percentile_diff(img) -> tuple:
+        import base64
+        import numpy as np
+        from io import BytesIO
+        
+        ACCEPTABLE_PERCENTILE = 99.5
+
+        expected_img_rgb = Image.open(img).convert("RGB")
+        test_uri_str = fid.convert_pil_to_base64(fid.get_img(img))
+        test_b64_encoding = test_uri_str.partition(",")[2]
+        test_img_rgb = Image.open(BytesIO(base64.b64decode(test_b64_encoding))).convert("RGB")
+
+        expected_pixel_arr = np.array(expected_img_rgb, dtype=np.int16)
+        test_pixel_arr = np.array(test_img_rgb, dtype=np.int16)
+        pixel_diffs = np.abs(expected_pixel_arr - test_pixel_arr)
+
+        avg_diff = np.average(pixel_diffs)
+        percentile_diff = np.percentile(pixel_diffs, ACCEPTABLE_PERCENTILE)
+        return avg_diff, percentile_diff
 
     def test_identify_food(self):
         pass
@@ -78,6 +99,7 @@ class TestFoodID(unittest.TestCase):
     def image_hash(img: Image.Image):
         return hashlib.md5(img.tobytes()).hexdigest()
     
+    @staticmethod
     def get_text(file_loc: str):
         with open(file_loc, 'r') as file:
             text = file.read()
