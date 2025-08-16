1caf9104cf6c3c3dda5ed1ca63c63abff3f5e57c
ZacharyMcKinney <Zachary.Mckinney04@gmail.com>
2025-08-15 23:27:46 -0500

    Updated models and what they store

--- src/models.py
+++ src/models.py
@@ -3,19 +3,16 @@
 from dataclasses import dataclass
 
 @dataclass
-class Food_Item:
-    """Class for keeping track of a food's ID, name, and macros per serving"""
+class Food:
     food_id: int
     food: str
-    carbs: float
-    protein: float
-    fat: float
     
 @dataclass
-class Meal:
-    meal_id: int
+class Food_Entry:
+    entry_id: int
     user_id: int
-    food_items: list
+    food_id: int
+    bgl_delta: int
     
 @dataclass
 class User:
